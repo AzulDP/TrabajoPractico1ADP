4b9c89c6f9d72e4bfd6689e6bb6e69c04bf2afda
Azul De Pol <azul.depol@gmail.com>
2023-03-05 15:16:56 +0000

    maquete el ejercicio 9 falta testeo

--- js/9-ejercicio9.js
+++ js/9-ejercicio9.js
@@ -1,3 +1,21 @@
 // Dificultad:  🟢🟡 
 // 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
 // Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+// 'a' ||'A'||'e'||'E'||'i'||'I'||'o'||'O'||'u'||'U'
+
+const sentence = prompt('Escribe una frase')
+
+for (let vowels = 0; vowels <=sentence.length; vocal++) {
+    if (sentence.charAt(vowels) == 'a' || 'A'||
+        sentence.charAt(vowels) == 'e' || 'E'||
+        sentence.charAt(vowels) == 'i' ||'I'||
+        sentence.charAt(vowels) == 'o' ||'O'||
+        sentence.charAt(vowels) == 'u'||'U') {
+        document.write(`${sentence[vowels]}`)
+    } else if (sentence.charAt !=(vowels)) {
+        document.write(`Su frase no contiene vocales.`)
+    } 
+    else {
+        document.write(`Ingrese un valor válido`)
+    }
+}
