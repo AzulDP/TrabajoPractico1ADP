d37840724f24ede23934aaac993250fa69ee2fc4
Azul De Pol <azul.depol@gmail.com>
2023-03-07 18:25:43 +0000

    arregle el ejercicio 11

--- js/11-ejercicio11.js
+++ js/11-ejercicio11.js
@@ -6,19 +6,18 @@ if (number % 2 == 0 || number % 3 == 0 || number % 5 == 0 || number % 7 == 0) {
     if (number % 2 == 0) {
         document.write(`El número ${number} es divisible por 2`);
     }
-    <br></br>;
     if (number % 3 == 0) {
         document.write(`El número ${number} es divisible por 3`);
     }
-    <br></br>;
+
     if (number % 5 == 0) {
         document.write(`El número ${number} es divisible por 5`);
     }
-    <br></br>;
+
     if (number % 7 == 0) {
         document.write(`El número ${number} es divisible por 7`);
     }
-    <br></br>;
+
 }else {
     document.write(`El número no es divisible por 2, 3, 5 o 7`);
 }
